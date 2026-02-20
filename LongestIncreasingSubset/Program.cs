3c6fa71077c0114c0818fe2559f86876198ef5d3
SriGowri <srigowri.suresh@scientificgames.com>
2026-02-20 12:57:53 +0530

    Changes made for docker

--- LongestIncreasingSubset/Program.cs
+++ LongestIncreasingSubset/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using ILISService;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter integers separated by single whitespace:");
+        string? input = Console.ReadLine();
+
+        LISService lisService = new LISService();
+
+        var result = lisService.GetLongestIncreasingSubsequence(input ?? string.Empty);
+
+        Console.WriteLine("Longest Increasing Subsequence:");
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
